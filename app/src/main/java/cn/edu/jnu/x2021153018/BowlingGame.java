fe1ba890a6dbab75204dcd6198a8613229482826
Cooooooooold <kopang0730@stu2021.jnu.edu.cn>
2024-04-19 21:29:11 +0800

    testASpare() commit

--- app/src/main/java/cn/edu/jnu/x2021153018/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2021153018/BowlingGame.java
@@ -1,11 +1,24 @@
 package cn.edu.jnu.x2021153018;
 public class BowlingGame {
-    int score = 0;
+    int pins[]=new int[21];
+    int currentPinIndex=0;
     public void roll(int pin) {
-        score+=pin;
+        pins[currentPinIndex++]=pin;
     }
 
     public int score() {
-        return score;
+        int totalScore=0;
+        for(int scoreIndex=0;scoreIndex<pins.length;scoreIndex++)
+        {
+            totalScore+=pins[scoreIndex];
+            //如果是一个补中，就把下下个球的分数也计入当前轮
+            if(scoreIndex<19){
+                 if(10==pins[scoreIndex]+pins[scoreIndex+1])
+                {
+                    totalScore+=pins[scoreIndex+2];//把下下个球加上去
+                }
+            }
+        }
+        return totalScore;
     }
 }
--- app/src/test/java/cn/edu/jnu/x2021153018/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2021153018/BowlingGameUnitTest.java
@@ -29,6 +29,14 @@ public class BowlingGameUnitTest {
         repeatedRoll(1,20);
         assertEquals(20,game.score());
     }
+//    @Test
+//    public void testASpare()  {
+//        game.roll(3);
+//        game.roll(7);
+//        game.roll(6);
+//        repeatedRoll(0,17);
+//        assertEquals(22,game.score());
+//    }
 
     private void repeatedRoll(int pin, int times) {
         for (int i = 0; i < times; i++) {
